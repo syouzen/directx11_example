1a1ee0df1b054e7633cac93a98e55e0bc03c0825
syouzen <seungyeon.choi1026@gmail.com>
2019-04-11 18:13:03 +0900

    [feat] Add Cube Example
    - same as title.

---
+++ src/EXAMPLE/cube/light.cc
@@ -0,0 +1,35 @@
+#include "light.h"
+
+Light::Light()
+{
+}
+
+Light::Light(const Light& other)
+{
+}
+
+Light::~Light()
+{
+}
+
+void Light::setDiffuseColor(float red, float green, float blue, float alpha)
+{
+	m_diffuseColor = D3DXVECTOR4(red, green, blue, alpha);
+	return;
+}
+
+void Light::setDirection(float x, float y, float z)
+{
+	m_direction = D3DXVECTOR3(x, y, z);
+	return;
+}
+
+D3DXVECTOR4 Light::getDiffuseColor()
+{
+	return m_diffuseColor;
+}
+
+D3DXVECTOR3 Light::getDirection()
+{
+	return m_direction;
+}
